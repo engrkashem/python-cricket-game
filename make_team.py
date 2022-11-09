71e1aea61c29023f74e73b447a9c255120f14a3f
engrkashem <kashemaust@gmail.com>
2022-11-09 19:14:48 +0600

    team bd and ind fulfilled

--- make_team.py
+++ make_team.py
@@ -2,14 +2,18 @@ from player import Player
 from team_tournamets import Tournament, Team
 
 cup = Tournament()
+# Team Bangladesh Making
 bangladesh = Team('Bangladesh')
-india = Team('India')
-bd_players_list = ['Tamim Iqbal', 'Sakib Al Hasan', 'Mushfiqur Rahim',
-                   'Mustafizur Rahman', 'Taskin Ahmed']
+
+bd_players_list = ['Tamim Iqbal', 'Liton Das', 'Sakib Al Hasan', 'Md Ashraful', 'Mushfiqur Rahim',
+                   'Mohammadullah', 'Mehedi Meraz', 'Mosaddek Hossain', 'Mashrafee Mortaza', 'Mustafizur Rahman', 'Taskin Ahmed']
 for player_name in bd_players_list:
     player = Player(player_name, bangladesh)
-# tamim = Player('Tamim Iqbal', bangladesh)
-# sakib = Player('Sakib Al Hasan', bangladesh)
-kohli = Player('Virat Kohli', india)
-rohit = Player('Rohit Sharma', india)
-bumra = Player('Jasprit Bumra', india)
+
+# Team India Making
+india = Team('India')
+
+ind_players_list = ['Virat Kohli', 'Sachin Tendulkar', 'Sourab Ganguly', 'MS Dhoni', 'Yuvraj Shings',
+                    'Mohammad Yousuf', 'Hardik Pandiya', 'Mohammad Irfan', 'Jaspreet Bumrah', 'Bhuvoneshwar Kumar', 'Anil Kumble']
+for player_name in ind_players_list:
+    player = Player(player_name, india)
